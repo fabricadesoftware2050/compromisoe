77d409a5751d21eba515bf586354c8e95448958e
Christian Camilo Serna Gamez <christian.sernag@bam.com.gt>
2025-07-08 21:07:03 -0500

    Agrega seguidores crud filament

--- app/Filament/Panel/Resources/SeguidoresResource.php
+++ app/Filament/Panel/Resources/SeguidoresResource.php
@@ -19,7 +19,7 @@ class SeguidoresResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
         protected static ?string $pluralModelLabel = 'seguidores';
-    protected static ?string $modelLabel = 'seguidor';
+    protected static ?string $modelLabel = 'seguidores';
 
     public static function form(Form $form): Form
     {
