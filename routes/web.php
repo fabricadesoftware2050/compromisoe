8887ed005ddd91ea662492b1bf038ee56ee0ac24
Christian Camilo Serna Gamez <christian.sernag@bam.com.gt>
2025-07-08 20:02:30 -0500

    Agrega home

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 })->name('login');
+Route::get('/login', function () {
+    return view('welcome');
+});
 
 Route::post('/login', [AuthController::class,'login'])->name('auth.login');
 
