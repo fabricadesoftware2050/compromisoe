84f355a4a7b18b66a31001edee9d767ded4a2b44
Christian Camilo Serna Gamez <christian.sernag@bam.com.gt>
2025-07-08 21:05:25 -0500

    Agrega seguidores crud filament

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,8 +18,8 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-protected static ?string $pluralModelLabel = 'usuarios';
-protected static ?string $modelLabel = 'usuario';
+    protected static ?string $pluralModelLabel = 'usuarios';
+    protected static ?string $modelLabel = 'usuario';
     public static function form(Form $form): Form
     {
         return $form
